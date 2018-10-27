977cda8418d2d0b4954b67b75ac17b5d63e83692
rsangole <rahul.sangole@gmail.com>
2018-10-27 13:09:22 -0400

    Adds nzv, binning, factoring, pca functions

--- munge/01-C-FeatureEngg.R
+++ munge/01-C-FeatureEngg.R
@@ -3,6 +3,12 @@ library(tidyverse)
 library(lattice)
 library(latticeExtra)
 library(caret)
+library(ggthemr)
+library(janitor)
+library(gridExtra)
+library(tidyr)
+ggthemr('fresh')
+options(max.print = 1000)
 
 
 load('cache/raw_train.rdata')
@@ -20,6 +26,7 @@ raw_train[c("elevation","aspect","slope","horizontal_distance_to_hydrology","ver
 
 ## Only elevation has an estimation of lambda = 2
 add_boxbox_transform <- function(df){
+    message("... Box-Cox transforming `elevation` variable")
     df %>%
         mutate(elevation_boxcox = elevation^2) %>%
         dplyr::select(-elevation)
@@ -27,16 +34,123 @@ add_boxbox_transform <- function(df){
 
 
 # Binning continuous variables ----
+make_and_print_bins <- function(x, breaks=10){
+    x_cut <- cut(x, breaks = breaks)
+    print(x_cut %>% tabyl() %>% adorn_pct_formatting())
+    grid.arrange(histogram(~x),
+                 histogram(~x_cut,
+                           scales = list(x=list(rot=45))),
+                 ncol = 2)
+}
+make_and_print_bins(raw_train$elevation)
+make_and_print_bins(raw_train$aspect)
+make_and_print_bins(raw_train$slope)
+make_and_print_bins(raw_train$horizontal_distance_to_hydrology)
+make_and_print_bins(raw_train$vertical_distance_to_hydrology)
+make_and_print_bins(raw_train$horizontal_distance_to_roadways)
+make_and_print_bins(raw_train$hillshade_9am)
+make_and_print_bins(raw_train$hillshade_noon)
+make_and_print_bins(raw_train$hillshade_3pm)
+make_and_print_bins(raw_train$horizontal_distance_to_fire_points)
 
-
-
-
+transform_continuous_to_bins <- function(df, vars = NULL, breaks = 10){
+    message("... Transforming conti vars to binned factor vars, using breaks = ", breaks)
+    if(is.null(vars))
+        vars = c("elevation",
+                 "aspect",
+                 "slope",
+                 "horizontal_distance_to_hydrology",
+                 "vertical_distance_to_hydrology",
+                 "horizontal_distance_to_roadways",
+                 "hillshade_9am",
+                 "hillshade_noon",
+                 "hillshade_3pm",
+                 "horizontal_distance_to_fire_points")
+    split_mask <- names(df) %in% vars
+    df[split_mask] %>%
+        purrr::map_df(~cut(.x, breaks=breaks)) %>%
+        bind_cols(df[!split_mask])
+}
 
 # Factor variables to single column ----
+wilderness_areas <-
+    tbl_df(data.frame(
+        wa = stringr::str_c("wilderness_area", 1:4),
+        wilderness_area = c(
+            "Rawah Wilderness Area",
+            "Neota Wilderness Area",
+            "Comanche Peak Wilderness Area",
+            "Cache la Poudre Wilderness Area"
+        ),
+        stringsAsFactors = FALSE
+    ))
+soil_types <-
+    tbl_df(data.frame(
+        st = stringr::str_c("soil_type", 1:40),
+        soil_type = c(
+            "Cathedral family -Rock outcrop complex, extremely stony",
+            "Vanet -Ratake families complex, very stony",
+            "Haploborolis -Rock outcrop complex, rubbly",
+            "Ratake family -Rock outcrop complex, rubbly",
+            "Vanet family -Rock outcrop complex complex, rubbly",
+            "Vanet -Wetmore families -Rock outcrop complex, stony",
+            "Gothic family",
+            "Supervisor -Limber families complex",
+            "Troutville family, very stony",
+            "Bullwark -Catamount families -Rock outcrop complex, rubbly",
+            "Bullwark -Catamount families -Rock land complex, rubbly.",
+            "Legault family -Rock land complex, stony",
+            "Catamount family -Rock land -Bullwark family complex, rubbly",
+            "Pachic Argiborolis -Aquolis complex",
+            "unspecified in the USFS Soil and ELU Survey",
+            "Cryaquolis -Cryoborolis complex",
+            "Gateview family -Cryaquolis complex",
+            "Rogert family, very stony",
+            "Typic Cryaquolis -Borohemists complex",
+            "Typic Cryaquepts -Typic Cryaquolls complex",
+            "Typic Cryaquolls -Leighcan family, till substratum complex",
+            "Leighcan family, till substratum, extremely bouldery",
+            "Leighcan family, till substratum -Typic Cryaquolls complex",
+            "Leighcan family, extremely stony",
+            "Leighcan family, warm, extremely stony",
+            "Granile -Catamount families complex, very stony",
+            "Leighcan family, warm -Rock outcrop complex, extremely stony",
+            "Leighcan family -Rock outcrop complex, extremely stony",
+            "Como -Legault families complex, extremely stony",
+            "Como family -Rock land -Legault family complex, extremely stony",
+            "Leighcan -Catamount families complex, extremely stony",
+            "Catamount family -Rock outcrop -Leighcan family complex, extremely stony",
+            "Leighcan -Catamount families -Rock outcrop complex, extremely stony",
+            "Cryorthents -Rock land complex, extremely stony",
+            "Cryumbrepts -Rock outcrop -Cryaquepts complex",
+            "Bross family -Rock land -Cryumbrepts complex, extremely stony",
+            "Rock outcrop -Cryumbrepts -Cryorthents complex, extremely stony",
+            "Leighcan -Moran families -Cryaquolls complex, extremely stony",
+            "Moran family -Cryorthents -Leighcan family complex, extremely stony",
+            "Moran family -Cryorthents -Rock land complex, extremely stony"
+        ),
+        stringsAsFactors = FALSE
+    ))
 
+transform_wilderness_to_factor <- function(df) {
+    message("... Transforming wilderness to factor")
+    df %>%
+        tidyr::gather(wa, wa_flag, wilderness_area1:wilderness_area4) %>%
+        dplyr::filter(wa_flag != 0) %>%
+        left_join(wilderness_areas, by = "wa") %>%
+        dplyr::select(-wa, -wa_flag) %>%
+        mutate(wilderness_area = as.factor(wilderness_area))
+    }
 
-
-
+transform_soil_to_factor <- function(df) {
+    message("... Transforming soil to factor")
+    df %>%
+        gather(key = st, value = st_flag, soil_type1:soil_type40) %>%
+        dplyr::filter(st_flag != 0) %>%
+        left_join(soil_types, by = "st") %>%
+        dplyr::select(-st, -st_flag) %>%
+        mutate(soil_type = as.factor(soil_type))
+    }
 
 # Distances
 
@@ -63,17 +177,36 @@ add_boxbox_transform <- function(df){
 
 
 # PCA
-pca_fit <- prcomp(x = raw_train[1:14], center = T, scale. = T)
+## PCA for all components except Soil Type & Wilderness Type
+pca_fit <- prcomp(x = raw_train[1:10], center = T, scale. = T)
 pca_fit
 pca_fit %>% summary
-screeplot(pca_fit,type = 'l', npcs = 14)
-## Potentially keep ~8 PCs for all variables inserted in the PCA model (except Soil Type)
-## ~90% of variation explained
+screeplot(pca_fit,type = 'l', npcs = 10)
+## Potentially keep ~6 PCs for all variables inserted in the PCA model (except Soil Type)
+## ~89% of variation explained
 
-add_pca_transform <- function(df){
-    pca_df <- as_tibble(predict(pca_fit, raw_train[1:14])[,1:8])
-    df[,1:14] <- NULL
-    pca_df %>% bind_cols(df)
+add_pca_transform_continuous_vars <- function(df){
+    message("... All vars except Soil or Wild vars replaced by 6 PCA components")
+    pca_df <- as_tibble(predict(pca_fit, df %>% select("elevation","aspect","slope","horizontal_distance_to_hydrology","vertical_distance_to_hydrology","horizontal_distance_to_roadways","hillshade_9am","hillshade_noon","hillshade_3pm","horizontal_distance_to_fire_points"))[,1:6])
+    keep = !(names(df) %in% c("elevation","aspect","slope","horizontal_distance_to_hydrology","vertical_distance_to_hydrology","horizontal_distance_to_roadways","hillshade_9am","hillshade_noon","hillshade_3pm","horizontal_distance_to_fire_points"))
+    pca_df %>% bind_cols(df[keep])
 }
+
+## PCA for Soil Type & Wild Type
+pca_fit2 <- prcomp(x = raw_train %>% select(contains('wild'),contains('soil')), center = T, scale. = T)
+pca_fit2
+pca_fit2 %>% summary
+screeplot(pca_fit2,type = 'l', npcs = 20)
+## Doesn't seem to show any promise of variable reduction here
+## Though there is a sharp knee in the scree plot at k=4
+
+add_pca_transform_soil_wild <- function(df){
+    message("... Soil & Wild vars replaced by 4 PCA components")
+    pca_df <- as_tibble(predict(pca_fit2, raw_train %>% select(contains('wild'),contains('soil')))[,1:4])
+    keep = !(names(df) %in% names(df %>% dplyr::select(contains('wild'),contains('soil'))))
+    pca_df %>% bind_cols(df[keep])
+}
+add_pca_transform_soil_wild(raw_train)
+
 
 # Fully standardized training set
