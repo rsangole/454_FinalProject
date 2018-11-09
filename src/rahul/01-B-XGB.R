909f1c0ad2f33b1fa6928df7bebb68f0a34c3f7c
R Sangole <hw831@cummins.com>
2018-11-09 22:23:26 +0000

    Finishes model 1

--- src/rahul/01-B-XGB.R
+++ src/rahul/01-B-XGB.R
@@ -6,13 +6,16 @@ df_m1 <- df_xg %>%
 glimpse(df_m1)
 
 (tsk <- makeClassifTask(data = df_m1, target = "cover_type"))
-(lrn <- makeLearner(cl = "classif.xgboost", id = "model1_xbg"))
-(rdesc <- makeResampleDesc(method = "CV", iters = 3))
-ps = makeParamSet(
-    makeNumericParam("nrounds", lower = 20L, upper = 80L),
-    makeDiscreteParam("max_depth", values = c(2,4,6))
-)
-ctrl = makeTuneControlRandom(maxit = 10L)
+(lrn <- makeLearner(cl = "classif.xgboost", id = "model1_xbg", nrounds=300, verbose = 1))
+(rdesc <- makeResampleDesc(method = "CV", iters = 5))
+# ps = makeParamSet(
+#     makeDiscreteParam("nrounds", values = c(80L,120L,200L)),
+#     makeDiscreteParam("max_depth", values = c(2,4,6))
+# )
+# ctrl = makeTuneControlRandom(maxit = 10L)
+# ctrl = makeTuneControlGrid(resolution = 10)
+
+# parallelMap::parallelStart(mode = "multicore", cpus = 8)
 
 # (res <- resample(learner = lrn,
 #                  task = tsk,
@@ -20,8 +23,23 @@ ctrl = makeTuneControlRandom(maxit = 10L)
 #                  measures = acc,
 #                  show.info = T))
 
-res = tuneParams(learner = lrn,
-                 task = tsk,
-                 resampling = rdesc,
-                 par.set = ps,
-                 control = ctrl)
+# (res = tuneParams(learner = lrn,
+#                  task = tsk,
+#                  resampling = rdesc,
+#                  par.set = ps,
+#                  control = ctrl))
+
+(mod <- train(lrn, tsk))
+
+pred = predict(mod, task = tsk)
+head(getPredictionProbabilities(pred))
+
+calculateConfusionMatrix(pred)
+calculateConfusionMatrix(pred, relative = T)
+performance(pred)
+
+generateFilterValuesData(tsk)
+
+getLearnerModel(mod) %>%
+    xgb.importance(model = .) %>%
+    xgb.ggplot.importance(top_n = 10, rel_to_first = T)
