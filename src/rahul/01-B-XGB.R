f3203d82606c1ce73bc68d999dd5e164412ac9ed
R Sangole <hw831@cummins.com>
2018-11-24 03:13:30 +0000

    Adds more xbg modeling

--- src/rahul/01-B-XGB.R
+++ src/rahul/01-B-XGB.R
@@ -6,7 +6,7 @@ df_m1 <- df_xg %>%
 glimpse(df_m1)
 
 (tsk <- makeClassifTask(data = df_m1, target = "cover_type"))
-(lrn <- makeLearner(cl = "classif.xgboost", id = "model1_xbg", nrounds=300, verbose = 1))
+(lrn <- makeLearner(cl = "classif.xgboost", id = "model1_xbg", nrounds=400, verbose = 0, max_depth = 8))
 (rdesc <- makeResampleDesc(method = "CV", iters = 5))
 # ps = makeParamSet(
 #     makeDiscreteParam("nrounds", values = c(80L,120L,200L)),
@@ -34,7 +34,6 @@ glimpse(df_m1)
 pred = predict(mod, task = tsk)
 head(getPredictionProbabilities(pred))
 
-calculateConfusionMatrix(pred)
 calculateConfusionMatrix(pred, relative = T)
 performance(pred)
 
@@ -43,3 +42,8 @@ generateFilterValuesData(tsk)
 getLearnerModel(mod) %>%
     xgb.importance(model = .) %>%
     xgb.ggplot.importance(top_n = 10, rel_to_first = T)
+
+xgb.ggplot.deepness(getLearnerModel(mod))
+
+xgbFit <- getLearnerModel(mod)
+xgbFit$evaluation_log %>% plot
