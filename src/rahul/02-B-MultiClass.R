a4ec4e00de6a984d2e8cea051fa7a927d2b39ad8
rsangole <rahul.sangole@gmail.com>
2018-11-24 18:16:32 -0500

    Adds 2nd hyp

--- src/rahul/02-B-MultiClass.R
+++ src/rahul/02-B-MultiClass.R
@@ -11,15 +11,31 @@ meas_multi <- list(mmce, kappa)
 # (rdesc <- makeResampleDesc(method = "CV", iters = 10, stratify = TRUE))
 (rdesc <- makeResampleDesc(method = "Holdout", stratify = TRUE))
 
+tsk1 <- makeClassifTask(id = "base", data = df_hyp1_base_train, target = "cover_type")
+tsk2 <- makeClassifTask(id = "h1",   data = df_hyp1_train, target = "cover_type")
+
 #---- H0: Binning makes things better
-tsks <- list(
-    makeClassifTask(id = "base", data = df_hyp1_base_train, target = "cover_type"),
-    makeClassifTask(id = "h1",   data = df_hyp1_train, target = "cover_type")
-    )
-(lrn <- makeLearner(cl = "classif.xgboost", id = "model1_xbg", predict.type = "prob",
+tsks <- list(tsk1, tsk2)
+(lrn1 <- makeLearner(cl = "classif.xgboost", id = "model1_xbg", predict.type = "prob",
                     verbose = 1,
                     early_stopping_rounds = 20,
                     nrounds = 50L,
                     eta = 0.3,
                     max_depth = 10))
 bmr_h1 = benchmark(learners = lrn, tasks = tsks, resamplings = rdesc, measures = meas_multi, show.info = T)
+
+
+
+#---- 2* H0: PCA makes things better
+(lrn2 <- makePreprocWrapperCaret(learner = "classif.xgboost",
+                                id = "xgb_pca",
+                                predict.type = "prob",
+                                verbose = 1,
+                                early_stopping_rounds = 20,
+                                nrounds = 50L,
+                                eta = 0.3,
+                                max_depth = 10,
+                                ppc.pca = TRUE,
+                                ppc.thresh = 0.8))
+bmr_h2 = benchmark(learners = list(lrn1, lrn2), tasks = tsk1, resamplings = rdesc,
+                   measures = meas_multi, show.info = T)
