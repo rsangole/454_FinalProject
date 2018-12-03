17eaa0624386dd859b4d6c970f5989c631a78fbd
R Sangole <rsangole@u.northwestern.edu>
2018-12-03 00:16:44 +0000

    Adds watchlist to the mlr models

--- src/rahul/02-C-OneVsAll_Benchmark.R
+++ src/rahul/02-C-OneVsAll_Benchmark.R
@@ -1,6 +1,8 @@
 # Common definitions
 parallelMap::parallelStart(mode = "multicore")
-
+library(xgboost)
+library(mlr)
+library(tidyverse)
 source("munge/01-C-FeatureEngg.R")
 
 load("cache/raw_train.rdata")
@@ -42,40 +44,72 @@ df_xgbpkg_prep_test <- df_xgbpkg_prep_test %>%
                     verbose = 1, early_stopping_rounds = 3, nrounds = 300L))
 (meas_binary <- list(mmce, f1, kappa, fpr, fnr, auc))
 
+make_watchlist <- function(d1,d2){
+        train_data <- list(
+                data = as.matrix(d1[, -1]),
+                label = as.numeric(d1$cover_type) - 1)
+        test_data <- list(
+                data = as.matrix(d2[, -1]),
+                label = as.numeric(d2$cover_type) - 1)
+        dtrain <- xgb.DMatrix(data = train_data$data, label = train_data$label)
+        dtest <- xgb.DMatrix(data = test_data$data, label = test_data$label)
+        list(train = dtrain, test = dtest)
+}
 
 df_spruce <- df_xgbpkg_prep_train %>% make_response_var_one_vs_all("spruce_fir")
 df_spruce_test <- df_xgbpkg_prep_test %>% make_response_var_one_vs_all("spruce_fir")
 (tsk_spruce <- makeClassifTask(data = df_spruce, target = "cover_type"))
+(lrn <- makeLearner(cl = "classif.xgboost", id = "xbg", predict.type = "prob",
+                    verbose = 1, early_stopping_rounds = 3, nrounds = 300L,
+                    watchlist = make_watchlist(df_spruce, df_spruce_test)))
 fit_spruce <- train(lrn, tsk_spruce)
 
 df_lodgepole_pine <- df_xgbpkg_prep_train %>% make_response_var_one_vs_all("lodgepole_pine")
 df_lodgepole_pine_test <- df_xgbpkg_prep_test %>% make_response_var_one_vs_all("lodgepole_pine")
 (tsk_lodgepole_pine <- makeClassifTask(data = df_lodgepole_pine, target = "cover_type"))
+(lrn <- makeLearner(cl = "classif.xgboost", id = "xbg", predict.type = "prob",
+                    verbose = 1, early_stopping_rounds = 3, nrounds = 300L,
+                    watchlist = make_watchlist(df_lodgepole_pine, df_lodgepole_pine_test)))
 fit_lodgepole_pine <- train(lrn, tsk_lodgepole_pine)
 
 df_ponderosa_pine <- df_xgbpkg_prep_train %>% make_response_var_one_vs_all("ponderosa_pine")
 df_ponderosa_pine_test <- df_xgbpkg_prep_test %>% make_response_var_one_vs_all("ponderosa_pine")
 (tsk_ponderosa_pine <- makeClassifTask(data = df_ponderosa_pine, target = "cover_type"))
+(lrn <- makeLearner(cl = "classif.xgboost", id = "xbg", predict.type = "prob",
+                    verbose = 1, early_stopping_rounds = 3, nrounds = 300L,
+                    watchlist = make_watchlist(df_ponderosa_pine, df_ponderosa_pine_test)))
 fit_ponderosa_pine <- train(lrn, tsk_ponderosa_pine)
 
 df_cottonwood_Willow <- df_xgbpkg_prep_train %>% make_response_var_one_vs_all("cottonwood_Willow")
 df_cottonwood_Willow_test <- df_xgbpkg_prep_test %>% make_response_var_one_vs_all("cottonwood_Willow")
 (tsk_cottonwood_Willow <- makeClassifTask(data = df_cottonwood_Willow, target = "cover_type"))
+(lrn <- makeLearner(cl = "classif.xgboost", id = "xbg", predict.type = "prob",
+                    verbose = 1, early_stopping_rounds = 3, nrounds = 300L,
+                    watchlist = make_watchlist(df_cottonwood_Willow, df_cottonwood_Willow_test)))
 fit_cottonwood_Willow <- train(lrn, tsk_cottonwood_Willow)
 
 df_aspen <- df_xgbpkg_prep_train %>% make_response_var_one_vs_all("aspen")
 df_aspen_test <- df_xgbpkg_prep_test %>% make_response_var_one_vs_all("aspen")
 (tsk_aspen <- makeClassifTask(data = df_aspen, target = "cover_type"))
+(lrn <- makeLearner(cl = "classif.xgboost", id = "xbg", predict.type = "prob",
+                    verbose = 1, early_stopping_rounds = 3, nrounds = 300L,
+                    watchlist = make_watchlist(df_aspen, df_aspen_test)))
 fit_aspen <- train(lrn, tsk_aspen)
 
 df_douglasfir <- df_xgbpkg_prep_train %>% make_response_var_one_vs_all("douglasfir")
 df_douglasfir_test <- df_xgbpkg_prep_test %>% make_response_var_one_vs_all("douglasfir")
 (tsk_douglasfir <- makeClassifTask(data = df_douglasfir, target = "cover_type"))
+(lrn <- makeLearner(cl = "classif.xgboost", id = "xbg", predict.type = "prob",
+                    verbose = 1, early_stopping_rounds = 10, nrounds = 300L,
+                    watchlist = make_watchlist(df_douglasfir, df_douglasfir_test)))
 fit_douglasfir <- train(lrn, tsk_douglasfir)
 
 df_krummholz <- df_xgbpkg_prep_train %>% make_response_var_one_vs_all("krummholz")
 df_krummholz_test <- df_xgbpkg_prep_test %>% make_response_var_one_vs_all("krummholz")
 (tsk_krummholz <- makeClassifTask(data = df_krummholz, target = "cover_type"))
+(lrn <- makeLearner(cl = "classif.xgboost", id = "xbg", predict.type = "prob",
+                    verbose = 1, early_stopping_rounds = 3, nrounds = 300L,
+                    watchlist = make_watchlist(df_krummholz, df_krummholz_test)))
 fit_krummholz <- train(lrn, tsk_krummholz)
 
 
@@ -113,8 +147,9 @@ results <- tibble(
         ) %>%
         arrange(auc,kappa,mmce)
 results %>%
-        ggplot(aes(y=auc,x=class))+
-        geom_point()+
-        coord_flip()
+        ggplot(aes(y=kappa,x=auc))+
+        geom_point(aes(color=class))
 
 save(results, file = "src/rahul/cache/02_c_results.RData")
+
+#Result --> multiclass does better than this 1vall
