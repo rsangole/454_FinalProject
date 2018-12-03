f584d6592bbf4c1777492202a60eb29445937d21
R Sangole <rsangole@u.northwestern.edu>
2018-12-03 01:54:43 +0000

    Finished 1vA analysis

--- src/rahul/02-C-OneVsAll_Benchmark.R
+++ src/rahul/02-C-OneVsAll_Benchmark.R
@@ -60,7 +60,7 @@ df_spruce <- df_xgbpkg_prep_train %>% make_response_var_one_vs_all("spruce_fir")
 df_spruce_test <- df_xgbpkg_prep_test %>% make_response_var_one_vs_all("spruce_fir")
 (tsk_spruce <- makeClassifTask(data = df_spruce, target = "cover_type"))
 (lrn <- makeLearner(cl = "classif.xgboost", id = "xbg", predict.type = "prob",
-                    verbose = 1, early_stopping_rounds = 3, nrounds = 300L,
+                    verbose = 1, early_stopping_rounds = 10, nrounds = 300L,
                     watchlist = make_watchlist(df_spruce, df_spruce_test)))
 fit_spruce <- train(lrn, tsk_spruce)
 
@@ -68,7 +68,7 @@ df_lodgepole_pine <- df_xgbpkg_prep_train %>% make_response_var_one_vs_all("lodg
 df_lodgepole_pine_test <- df_xgbpkg_prep_test %>% make_response_var_one_vs_all("lodgepole_pine")
 (tsk_lodgepole_pine <- makeClassifTask(data = df_lodgepole_pine, target = "cover_type"))
 (lrn <- makeLearner(cl = "classif.xgboost", id = "xbg", predict.type = "prob",
-                    verbose = 1, early_stopping_rounds = 3, nrounds = 300L,
+                    verbose = 1, early_stopping_rounds = 10, nrounds = 300L,
                     watchlist = make_watchlist(df_lodgepole_pine, df_lodgepole_pine_test)))
 fit_lodgepole_pine <- train(lrn, tsk_lodgepole_pine)
 
@@ -76,7 +76,7 @@ df_ponderosa_pine <- df_xgbpkg_prep_train %>% make_response_var_one_vs_all("pond
 df_ponderosa_pine_test <- df_xgbpkg_prep_test %>% make_response_var_one_vs_all("ponderosa_pine")
 (tsk_ponderosa_pine <- makeClassifTask(data = df_ponderosa_pine, target = "cover_type"))
 (lrn <- makeLearner(cl = "classif.xgboost", id = "xbg", predict.type = "prob",
-                    verbose = 1, early_stopping_rounds = 3, nrounds = 300L,
+                    verbose = 1, early_stopping_rounds = 10, nrounds = 300L,
                     watchlist = make_watchlist(df_ponderosa_pine, df_ponderosa_pine_test)))
 fit_ponderosa_pine <- train(lrn, tsk_ponderosa_pine)
 
@@ -84,7 +84,7 @@ df_cottonwood_Willow <- df_xgbpkg_prep_train %>% make_response_var_one_vs_all("c
 df_cottonwood_Willow_test <- df_xgbpkg_prep_test %>% make_response_var_one_vs_all("cottonwood_Willow")
 (tsk_cottonwood_Willow <- makeClassifTask(data = df_cottonwood_Willow, target = "cover_type"))
 (lrn <- makeLearner(cl = "classif.xgboost", id = "xbg", predict.type = "prob",
-                    verbose = 1, early_stopping_rounds = 3, nrounds = 300L,
+                    verbose = 1, early_stopping_rounds = 10, nrounds = 300L,
                     watchlist = make_watchlist(df_cottonwood_Willow, df_cottonwood_Willow_test)))
 fit_cottonwood_Willow <- train(lrn, tsk_cottonwood_Willow)
 
@@ -92,7 +92,7 @@ df_aspen <- df_xgbpkg_prep_train %>% make_response_var_one_vs_all("aspen")
 df_aspen_test <- df_xgbpkg_prep_test %>% make_response_var_one_vs_all("aspen")
 (tsk_aspen <- makeClassifTask(data = df_aspen, target = "cover_type"))
 (lrn <- makeLearner(cl = "classif.xgboost", id = "xbg", predict.type = "prob",
-                    verbose = 1, early_stopping_rounds = 3, nrounds = 300L,
+                    verbose = 1, early_stopping_rounds = 10, nrounds = 300L,
                     watchlist = make_watchlist(df_aspen, df_aspen_test)))
 fit_aspen <- train(lrn, tsk_aspen)
 
@@ -108,7 +108,7 @@ df_krummholz <- df_xgbpkg_prep_train %>% make_response_var_one_vs_all("krummholz
 df_krummholz_test <- df_xgbpkg_prep_test %>% make_response_var_one_vs_all("krummholz")
 (tsk_krummholz <- makeClassifTask(data = df_krummholz, target = "cover_type"))
 (lrn <- makeLearner(cl = "classif.xgboost", id = "xbg", predict.type = "prob",
-                    verbose = 1, early_stopping_rounds = 3, nrounds = 300L,
+                    verbose = 1, early_stopping_rounds = 10, nrounds = 300L,
                     watchlist = make_watchlist(df_krummholz, df_krummholz_test)))
 fit_krummholz <- train(lrn, tsk_krummholz)
 
@@ -123,7 +123,7 @@ pred__krummholz_test<- predict(fit_krummholz, newdata = df_krummholz_test)
 
 results <- tibble(
         class = c(
-                "spruce",
+                "spruce_fir",
                 "lodgepole_pine",
                 "ponderosa_pine",
                 "cottonwood_Willow",
@@ -143,7 +143,9 @@ results <- tibble(
                 perf = map(predictions, ~performance(.x, meas_binary)),
                 mmce = map_dbl(perf, ~.x[["mmce"]]),
                 auc = map_dbl(perf, ~.x[["auc"]]),
-                kappa = map_dbl(perf, ~.x[["kappa"]])
+                kappa = map_dbl(perf, ~.x[["kappa"]]),
+                pred_class = map(predictions, ~getPredictionResponse(.x)),
+                pred_prob = map(predictions, ~getPredictionProbabilities(.x))
         ) %>%
         arrange(auc,kappa,mmce)
 results %>%
@@ -153,3 +155,63 @@ results %>%
 save(results, file = "src/rahul/cache/02_c_results.RData")
 
 #Result --> multiclass does better than this 1vall
+
+
+calculateROCMeasures(pred__lodgepole_pine_test)
+calculateROCMeasures(pred__ponderosa_pine_test)
+calculateROCMeasures(pred__cottonwood_Willow_test)
+calculateROCMeasures(pred__aspen_test)
+calculateROCMeasures(pred__douglasfir_test)
+calculateROCMeasures(pred__krummholz_test)
+
+
+# Early stopping = 3
+# > results
+# # A tibble: 7 x 6
+# class             predictions             perf         mmce   auc kappa
+# <chr>             <list>                  <list>      <dbl> <dbl> <dbl>
+#         1 spruce            <S3: PredictionClassif> <dbl [6]> 0.0913  0.970 0.802
+# 2 lodgepole_pine    <S3: PredictionClassif> <dbl [6]> 0.0880  0.973 0.824
+# 3 aspen             <S3: PredictionClassif> <dbl [6]> 0.00519 0.998 0.822
+# 4 douglasfir        <S3: PredictionClassif> <dbl [6]> 0.00568 0.999 0.900
+# 5 ponderosa_pine    <S3: PredictionClassif> <dbl [6]> 0.00582 0.999 0.949
+# 6 cottonwood_Willow <S3: PredictionClassif> <dbl [6]> 0.00111 0.999 0.878
+# 7 krummholz         <S3: PredictionClassif> <dbl [6]> 0.00305 1.000 0.955
+# >
+
+
+combined_results <- as_tibble(matrix(ncol = 7, nrow = pred__spruce_test$data %>% nrow))
+for (i in 1:7) {
+        combined_results[,i] = results$pred_prob[[i]]
+}
+names(combined_results) <- results$class
+combined_results
+
+combined_results <- combined_results %>%
+        mutate(max_pred = colnames(combined_results)[apply(combined_results,1,which.max)])
+
+combined_results$max_pred = factor(combined_results$max_pred,
+                                   levels = levels(df_xgbpkg_prep_test$cover_type))
+
+caret::confusionMatrix(combined_results$max_pred, df_xgbpkg_prep_test$cover_type)
+# Confusion Matrix and Statistics
+#
+# Reference
+# Prediction          spruce_fir lodgepole_pine ponderosa_pine cottonwood_Willow aspen douglasfir krummholz
+# spruce_fir             57501           3837              0                 0    25          4       310
+# lodgepole_pine          5864          80732            225                 1   610        195        33
+# ponderosa_pine             1            129          10195                62    23        270         0
+# cottonwood_Willow          0              0             42               740     0         26         0
+# aspen                     29            138              7                 0  2176          5         1
+# douglasfir                10            136            257                21    12       4710         0
+# krummholz                147             18              0                 0     1          0      5809
+#
+# Overall Statistics
+#
+# Accuracy : 0.9286
+# 95% CI : (0.9274, 0.9298)
+# No Information Rate : 0.4876
+# P-Value [Acc > NIR] : < 2.2e-16
+#
+# Kappa : 0.8848
+# Mcnemar's Test P-Value : NA
